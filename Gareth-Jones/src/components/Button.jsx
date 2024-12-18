c3a4bf2511208b67cdfec401b916641014d97882
Gareth-Jones <garethjonescode@gmail.com>
2024-12-19 01:50:52 +1300

    nearly finished hero

---
+++ Gareth-Jones/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+const ButtonPrimary = ({ href, target = '_self', label, icon, classes }) => {
+  return <div>Button</div>
+}
+
+ButtonPrimary.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+}
+
+export { ButtonPrimary }
