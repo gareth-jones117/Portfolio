b874f9cf529407564a8c77a57dbaaef2e0280b7a
Gareth-Jones <garethjonescode@gmail.com>
2024-12-19 15:57:47 +1300

    made about, intial commit

--- Gareth-Jones/src/App.jsx
+++ Gareth-Jones/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <main>
         <Hero />
       </main>
+      <About />
     </>
   )
 }
