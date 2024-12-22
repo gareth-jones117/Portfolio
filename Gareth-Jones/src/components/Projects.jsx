7f7fa371297106584b8716bc30869a25b2d062bc
Gareth Jones <garethjonescode@gmail.com>
2024-12-22 13:26:05 +1300

    finished projects and projects stuff

--- Gareth-Jones/src/components/Projects.jsx
+++ Gareth-Jones/src/components/Projects.jsx
@@ -1,20 +1,39 @@
+import ProjectCard from './ProjectCard'
+
 const project = [
   {
     imgSrc: 'src/assets/Stellar-share.png',
     title: 'Stellar-Share',
-    tags: ['Fullstack', 'Web Development', 'API'],
+    tags: ['Fullstack' + ' ', 'Web Development' + ' ', 'API'],
     projectLink: 'https://stellar-share-mania-roa-24.pushed.nz/',
   },
   {
-    imgSrc: '',
-    title: '',
-    tags: ['Fullstack', 'Web Development', 'API'],
-    projectLink: 'https://stellar-share-mania-roa-24.pushed.nz/',
+    imgSrc: 'src/assets/Snake.png',
+    title: 'Snake',
+    tags: ['Game' + ' ', 'HTML' + ' ', 'Javascript'],
+    projectLink: 'https://gareth-jones117.github.io/Snake/',
   },
 ]
 
 const Projects = () => {
-  return <div>Projects</div>
+  return (
+    <section className="section" id="projects">
+      <div className="container">
+        <h2 className="headline-2 mb-8">Some Of My Projects</h2>
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minimax(280px,_1fr))]">
+          {project.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Projects
